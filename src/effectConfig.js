da4bb8719e5c7c61e760f791225881eb7c53d620
Ossama <skymen75019@gmail.com>
2023-08-28 19:44:22 +0200

    1.0.0.2

--- src/effectConfig.js
+++ src/effectConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   addonType: "effect",
   id: "skymen_RoundedSquareMask",
   name: "Rounded Square Mask",
-  version: "1.0.0.1",
+  version: "1.0.0.2",
   category: "blend",
   // "distortion",
   // "normal-mapping",
